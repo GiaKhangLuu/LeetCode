91041035a9a65ba98e2bedd5e8bfe8369460b013
GiaKhangLuu <waoknight@gmail.com>
2023-12-03 10:14:42 +0800

    complete binary_search

---
+++ array/binary_search/sol_binary_search.py
@@ -0,0 +1,41 @@
+import json
+
+color_code = {'green': '\033[92m',
+              'red': '\033[91m',
+              'end': '\033[0m'}
+
+class Solution:
+    def search(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+
+        return -1
+
+    def test(self, func, inp, expectation):
+        nums, target = inp['nums'], inp['target']
+        out = func(nums, target)
+
+        if out == expectation:
+            print('{}Correct{}'.format(color_code['green'], color_code['end']))
+        else:
+            print('{}Wrong{}'.format(color_code['red'], color_code['end']))
+            print('{}Expectation: {}{}'.format(color_code['red'], expectation, color_code['end']))
+            print('{}Output: {}{}'.format(color_code['red'], out, color_code['end']))
+
+if __name__ == '__main__':
+    file_test_case = open('./data_binary_search.json')
+    test_case = json.load(file_test_case)
+    solution = Solution()
+
+    for i in range(len(test_case)):
+        inp, expectation = list(test_case[i].values())
+
+        print('>>>>>>>>>>> Test case: {} <<<<<<<<<<'.format(i + 1))
+        solution.test(solution.search, inp, expectation)
